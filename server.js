2fdc6e35795650d97ef228e982efc3a7f83fa8bb
Laura Martinez <laura.martinez@10pearls.com>
2020-05-04 09:57:15 -0500

    prepared for deployment

--- server.js
+++ server.js
@@ -10,15 +10,13 @@ connectDB();
 // Init middlaware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("Api running"));
-
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
 //server static
-if (process.env.NODE_ENV === "productio") {
+if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 
     app.get("*", (req, res) => {
